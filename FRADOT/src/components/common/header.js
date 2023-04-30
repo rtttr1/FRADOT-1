aa2e77c3c3f4414b9281552f07846e8cfa36c5cd
hutg89k <hutg89k@naver.com>
2023-04-30 13:45:18 +0900

    header 축소 해도 가운데 유지

--- FRADOT/src/components/common/header.js
+++ FRADOT/src/components/common/header.js
@@ -1,27 +1,35 @@
-import { HeaderWrap, Img, Logo, Menu2 } from "@/styles/common/header.styles";
+import {
+  HeaderDiv,
+  HeaderWrap,
+  Img,
+  Logo,
+  Menu2,
+} from "@/styles/common/header.styles";
 
 const Header = () => {
-    return (
-        <HeaderWrap>
-            <Logo>
-                <a href="/">
-                    {/* <image src={h} width={238} height={59} alt="프라닷" /> */}
-                    {/* FRADOT */}
-                    <Img></Img>
-                </a>
-            </Logo>
+  return (
+    <HeaderWrap>
+      <HeaderDiv>
+        <Logo>
+          <a href="/">
+            {/* <image src={h} width={238} height={59} alt="프라닷" /> */}
+            {/* FRADOT */}
+            <Img></Img>
+          </a>
+        </Logo>
 
-            <Menu2>
-                <a href="#">동네찾기</a>
-            </Menu2>
-            <Menu2>
-                <a href="#">인프라 찾기</a>
-            </Menu2>
-            <Menu2>
-                <a href="#">About us</a>
-            </Menu2>
-        </HeaderWrap>
-    );
+        <Menu2>
+          <a href="#">동네찾기</a>
+        </Menu2>
+        <Menu2>
+          <a href="#">인프라 찾기</a>
+        </Menu2>
+        <Menu2>
+          <a href="#">About us</a>
+        </Menu2>
+      </HeaderDiv>
+    </HeaderWrap>
+  );
 };
 
 export default Header;
