035319e06e66dc9f3e53c137146320c4004ab501
hutg89k <hutg89k@naver.com>
2023-05-25 18:51:26 +0900

    map페이지 헤더추가

---
+++ FRADOT/src/components/common/mapHeader.js
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import {
+  HeaderDiv,
+  HeaderWrap,
+  Img,
+  Menu2,
+} from "@/styles/common/mapHeader.styles";
+
+const mapHeader = () => {
+  const router = useRouter();
+
+  const homeBtn = () => {
+    router.push("/");
+  };
+
+  const mapBtn = () => {
+    router.push("/map1");
+  };
+
+  const aboutBtn = () => {
+    router.push("/aboutUs");
+  };
+
+  return (
+    <HeaderWrap>
+      <HeaderDiv>
+        <Img onClick={homeBtn}></Img>
+
+        <Menu2>
+          <p onClick={mapBtn}>동네찾기</p>
+        </Menu2>
+        <Menu2>
+          <p>인프라 찾기</p>
+        </Menu2>
+        <Menu2>
+          <p onClick={aboutBtn}>About us</p>
+        </Menu2>
+      </HeaderDiv>
+    </HeaderWrap>
+  );
+};
+
+export default mapHeader;
